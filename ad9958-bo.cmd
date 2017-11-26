a01f77919644b37f41f2b5c823b25fe6872c1353
Newell Jensen <newell.jensen@gmail.com>
2017-11-26 11:13:17 -0800

    Update README.md, add datasheets, schematics, board, and images.

---
+++ ad9958-bo.cmd
@@ -0,0 +1,271 @@
+# Pin name action command file
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element C19
+ChangePinName(C19, 2, 2)
+ChangePinName(C19, 1, 1)
+
+# Start of element C18
+ChangePinName(C18, 2, 2)
+ChangePinName(C18, 1, 1)
+
+# Start of element C17
+ChangePinName(C17, 2, 2)
+ChangePinName(C17, 1, 1)
+
+# Start of element C16
+ChangePinName(C16, 2, 2)
+ChangePinName(C16, 1, 1)
+
+# Start of element C15
+ChangePinName(C15, 2, 2)
+ChangePinName(C15, 1, 1)
+
+# Start of element C14
+ChangePinName(C14, 2, 2)
+ChangePinName(C14, 1, 1)
+
+# Start of element C13
+ChangePinName(C13, 2, 2)
+ChangePinName(C13, 1, 1)
+
+# Start of element C12
+ChangePinName(C12, 2, 2)
+ChangePinName(C12, 1, 1)
+
+# Start of element C11
+ChangePinName(C11, 2, 2)
+ChangePinName(C11, 1, 1)
+
+# Start of element C10
+ChangePinName(C10, 2, 2)
+ChangePinName(C10, 1, 1)
+
+# Start of element C9
+ChangePinName(C9, 2, 2)
+ChangePinName(C9, 1, 1)
+
+# Start of element C8
+ChangePinName(C8, 2, 2)
+ChangePinName(C8, 1, 1)
+
+# Start of element C7
+ChangePinName(C7, 2, 2)
+ChangePinName(C7, 1, 1)
+
+# Start of element C6
+ChangePinName(C6, 2, 2)
+ChangePinName(C6, 1, 1)
+
+# Start of element C5
+ChangePinName(C5, 2, 2)
+ChangePinName(C5, 1, 1)
+
+# Start of element C4
+ChangePinName(C4, 2, 2)
+ChangePinName(C4, 1, 1)
+
+# Start of element C20
+ChangePinName(C20, 2, 2)
+ChangePinName(C20, 1, 1)
+
+# Start of element C3
+ChangePinName(C3, 2, 2)
+ChangePinName(C3, 1, 1)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element BNC1
+ChangePinName(BNC1, 1, 1)
+ChangePinName(BNC1, 2, 2)
+
+# Start of element LPF1
+
+# Start of element BNC0
+ChangePinName(BNC0, 1, 1)
+ChangePinName(BNC0, 2, 2)
+
+# Start of element LPF0
+
+# Start of element R5
+ChangePinName(R5, 1, 1)
+ChangePinName(R5, 2, 2)
+
+# Start of element R6
+ChangePinName(R6, 1, 1)
+ChangePinName(R6, 2, 2)
+
+# Start of element T1
+ChangePinName(T1, 6, 6)
+ChangePinName(T1, 5, 5)
+ChangePinName(T1, 4, 4)
+ChangePinName(T1, 3, 3)
+ChangePinName(T1, 2, 2)
+ChangePinName(T1, 1, 1)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element T0
+ChangePinName(T0, 6, 6)
+ChangePinName(T0, 5, 5)
+ChangePinName(T0, 4, 4)
+ChangePinName(T0, 3, 3)
+ChangePinName(T0, 2, 2)
+ChangePinName(T0, 1, 1)
+
+# Start of element R8
+ChangePinName(R8, 1, 1)
+ChangePinName(R8, 2, 2)
+
+# Start of element R7
+ChangePinName(R7, 1, 1)
+ChangePinName(R7, 2, 2)
+
+# Start of element P0
+ChangePinName(P0, 1, 1)
+
+# Start of element P1
+ChangePinName(P1, 1, 1)
+
+# Start of element P2
+ChangePinName(P2, 1, 1)
+
+# Start of element P3
+ChangePinName(P3, 1, 1)
+
+# Start of element VDD
+ChangePinName(VDD, 1, 1)
+
+# Start of element GND
+ChangePinName(GND, 1, 1)
+
+# Start of element I/O_UPDATE
+ChangePinName(I/O_UPDATE, 1, 1)
+
+# Start of element \_CS\_
+ChangePinName(\_CS\_, 1, 1)
+
+# Start of element SCLK
+ChangePinName(SCLK, 1, 1)
+
+# Start of element DVDD_I/O
+ChangePinName(DVDD_I/O, 1, 1)
+
+# Start of element SDIO_0
+ChangePinName(SDIO_0, 1, 1)
+
+# Start of element SDIO_1
+ChangePinName(SDIO_1, 1, 1)
+
+# Start of element SDIO_2
+ChangePinName(SDIO_2, 1, 1)
+
+# Start of element SDIO_3
+ChangePinName(SDIO_3, 1, 1)
+
+# Start of element CMS
+ChangePinName(CMS, 1, 1)
+
+# Start of element PDWN
+ChangePinName(PDWN, 1, 1)
+
+# Start of element RESET
+ChangePinName(RESET, 1, 1)
+
+# Start of element SYNC_OUT
+ChangePinName(SYNC_OUT, 1, 1)
+
+# Start of element SYNC_CLK
+ChangePinName(SYNC_CLK, 1, 1)
+
+# Start of element SYNC_IN
+ChangePinName(SYNC_IN, 1, 1)
+
+# Start of element C23
+ChangePinName(C23, 2, 2)
+ChangePinName(C23, 1, 1)
+
+# Start of element C22
+ChangePinName(C22, 2, 2)
+ChangePinName(C22, 1, 1)
+
+# Start of element C21
+ChangePinName(C21, 2, 2)
+ChangePinName(C21, 1, 1)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element X1
+ChangePinName(X1, 2, 2)
+ChangePinName(X1, 1, 1)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element U1
+ChangePinName(U1, 56, DGND)
+ChangePinName(U1, 55, DVDD)
+ChangePinName(U1, 54, SYNC_CLK)
+ChangePinName(U1, 53, SDIO_3)
+ChangePinName(U1, 52, SDIO_2)
+ChangePinName(U1, 51, SDIO_1)
+ChangePinName(U1, 50, SDIO_0)
+ChangePinName(U1, 49, DVDD_I/O)
+ChangePinName(U1, 48, SCLK)
+ChangePinName(U1, 47, \_CS\_)
+ChangePinName(U1, 46, I/O_UPDATE)
+ChangePinName(U1, 45, DVDD)
+ChangePinName(U1, 44, DGND)
+ChangePinName(U1, 43, P3)
+ChangePinName(U1, 42, P2)
+ChangePinName(U1, 41, P1)
+ChangePinName(U1, 40, P0)
+ChangePinName(U1, 39, AVDD)
+ChangePinName(U1, 37, AVDD)
+ChangePinName(U1, 36, AVDD)
+ChangePinName(U1, 35, AVDD)
+ChangePinName(U1, 33, AVDD)
+ChangePinName(U1, 31, AVDD)
+ChangePinName(U1, 30, AVDD)
+ChangePinName(U1, 29, AVDD)
+ChangePinName(U1, 27, LOOP_FILTER)
+ChangePinName(U1, 26, AVDD)
+ChangePinName(U1, 25, AGND)
+ChangePinName(U1, 24, CLK_MODE_SEL)
+ChangePinName(U1, 23, REF_CLK)
+ChangePinName(U1, 22, \_REF_CLK\_)
+ChangePinName(U1, 21, AVDD)
+ChangePinName(U1, 20, AGND)
+ChangePinName(U1, 19, AVDD)
+ChangePinName(U1, 18, AGND)
+ChangePinName(U1, 17, DAC_RSET)
+ChangePinName(U1, 16, AGND)
+ChangePinName(U1, 15, AVDD)
+ChangePinName(U1, 14, \_CH1_IOUT\_)
+ChangePinName(U1, 13, CH1_IOUT)
+ChangePinName(U1, 12, AGND)
+ChangePinName(U1, 11, AVDD)
+ChangePinName(U1, 10, AGND)
+ChangePinName(U1, 9, \_CHO_IOUT\_)
+ChangePinName(U1, 8, CHO_IOUT)
+ChangePinName(U1, 7, AVDD)
+ChangePinName(U1, 6, AGND)
+ChangePinName(U1, 5, AVDD)
+ChangePinName(U1, 4, PWR_DWN_CTL)
+ChangePinName(U1, 3, MASTER_RESET)
+ChangePinName(U1, 2, SYNC_OUT)
+ChangePinName(U1, 1, SYNC_IN)
